88ddb81f4aed704191860493f14de115dd2bdb0f
hddare <hddare@syr.edu>
2024-12-08 17:51:08 -0500

    in progress

--- code/tully_scraper.py
+++ code/tully_scraper.py
@@ -10,9 +10,16 @@ def tullyscraper(playwright: Playwright) -> None:
     page = context.new_page()
     page.goto("https://www.tullysgoodtimes.com/menus/")
 
-    # TODO Write code here
-    
-    # ---------------------
+    extracted_menu_items = []
+    for title in page.query_selector_all("h3.foodmenu__menu-section-title"):
+        title_text = title.inner_text()
+        print(title_text)
+        row = title.query_selector("~ *").query_selector("~ *")
+        for item in title.query_selector_all("div.foodmenu__item"):
+            item_text = item.inner_text()
+            extracted_menu_items.append(extract_menu_item(title_text, item_text))
+    menu_items = pd.DataFrame(extracted_menu_items)
+    menu_items.to_csv("tullys_menu.csv", index=False)
     context.close()
     browser.close()
 
